2653772c8229ba869ebd0bbd8c8faec7109e6e43
Ulises <ucapistranmtz@gmail.com>
2025-05-16 12:49:11 -0500

    comments

--- 1-javascript/config/db.js
+++ 1-javascript/config/db.js
@@ -2,7 +2,7 @@ const mongoose= require('mongoose');
 
 const connectDb = async ()=>{
     try {
-        
+        //!replace for an environment variable
         await mongoose.connect('mongodb://root:example@localhost:27017')
             console.log('MongoDB connected');
     } catch (error) {
