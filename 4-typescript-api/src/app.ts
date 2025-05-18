57c51bb76cf47c18ea0724ee5f5057b57ced977f
Ulises Capistrán <ucapistranmtz@gmail.com>
2025-05-17 21:37:58 -0500

    Adding helmet,cors,express-rate-limit middlewares

--- 4-typescript-api/src/app.ts
+++ 4-typescript-api/src/app.ts
@@ -2,10 +2,47 @@
 import './config/loadEnv';
 
 import express from 'express';
+import helmet from 'helmet';
+import rateLimit from 'express-rate-limit';;
+import cors from 'cors';
 import connectDb from './config/db'
 
 // doing the server preparations
 const server = express();
+// helmet is a middleware that helps to secure Express apps by setting various HTTP headers
+// it helps to protect the app from well-known vulnerabilities by setting HTTP headers
+// such as Content Security Policy, X-Content-Type-Options, X-Frame-Options, etc.
+// it helps to prevent attacks such as cross-site scripting (XSS), clickjacking, and other code injection attacks
+// it is a good practice to use helmet in production apps
+// it is not necessary to use helmet in development mode
+// but it is a good practice to use it in production mode
+server.use(helmet());
+
+// cors is a middleware that helps to enable Cross-Origin Resource Sharing (CORS)
+// it allows the server to specify which origins are allowed to access the resources
+// it is a security feature that restricts web pages from making requests to a different domain than the one that served the web page
+// it is a good practice to use cors in production apps
+// it is not necessary to use cors in development mode
+// but it is a good practice to use it in production mode
+server.use(cors({
+  origin: process.env.CORS_ORIGIN || '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true,
+  maxAge: 3600,
+}));
+
+
+// Rate limiting middleware 
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // limit each IP to 100 requests per windowMs
+  message: 'Too many requests, please try again later.',
+  headers: true
+});
+server.use(limiter);
+
 //connecting to the database
 connectDb();
 
