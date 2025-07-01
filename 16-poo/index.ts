fcd1ffebd2a7a8a4370b499ae652bd152f25f57e
Ulises <ucapistranmtz@gmail.com>
2025-07-01 13:58:32 -0500

    end

--- 16-poo/index.ts
+++ 16-poo/index.ts
@@ -13,33 +13,39 @@ class Spot {
     public checkOut?: Date
   ) {}
 }
-class SpotCosts {
+class VehicleRate {
   constructor(public vehicleType: VehicleType, public rate: number) {}
 }
 
 class ParkingLot {
   private availableSpots: number;
   private usedSpots: Spot[] = [];
-  constructor(public totalSpots: number, public costs: SpotCosts[]) {
+  private rates: Record<VehicleType, number>;
+
+  constructor(
+    public totalSpots: number,
+    public rateEntries: { vehicleType: VehicleType; rate: number }[]
+  ) {
     this.availableSpots = totalSpots;
     // this.costs = [...costs, {}];
-    console.log("costs", this.costs);
+    // Build Record from input array
+    this.rates = rateEntries.reduce((acc, curr) => {
+      acc[curr.vehicleType] = curr.rate;
+      return acc;
+    }, {} as Record<VehicleType, number>);
   }
 
   emptySpots = () => {
     return this.availableSpots - this.usedSpots.length;
   };
 
-  rate = async (vehicleType: VehicleType) => {
-    const cost = this.costs.find((cost) => cost.vehicleType === vehicleType);
-    if (!cost) {
-      return 10;
-    }
-    return cost.rate;
+  rate = (vehicleType: VehicleType) => {
+    const cost = this.rates[vehicleType];
+
+    return cost;
   };
 
-  checkIn = async (vehicle: Vehicle): Promise<boolean> => {
-    const checkinGranted: boolean = false;
+  checkIn = (vehicle: Vehicle) => {
     const alreadyParked: boolean = this.usedSpots.some(
       (spot) => spot.vehicle.plate.toLowerCase() === vehicle.plate.toLowerCase()
     ); //it means the user lost his previous ticket
@@ -54,14 +60,14 @@ class ParkingLot {
       const spot = new Spot(vehicle);
       this.usedSpots.push(spot);
       console.log(
-        `a vehicle with plates ${vehicle.plate} is parked now and it's a ${vehicle.vehicleType}`
+        `[check-In] a vehicle with plates ${vehicle.plate} is parked now and it's a ${vehicle.vehicleType}`
       );
+    } else {
+      throw new Error("Sorry there are no empty spots ");
     }
-
-    return checkinGranted;
   };
 
-  checkOut = async (vehicle: Vehicle) => {
+  checkOut = (vehicle: Vehicle) => {
     const parkedVehicle: Spot | undefined = this.usedSpots.find(
       (spot) => spot.vehicle.plate === vehicle.plate
     );
@@ -70,34 +76,80 @@ class ParkingLot {
         `It looks like you used a spot without accesing the system , please call our team support for guidance`
       );
     }
-    const rate = this.rate(vehicle.vehicleType);
+
     let hours = Math.ceil(
-      (new Date().getTime() - parkedVehicle.checkIn.getTime()) *
-        (1000 * 60 * 60)
+      (Date.now() - parkedVehicle.checkIn.getTime()) / (1000 * 60 * 60)
     );
     hours = hours < 1 ? 1 : hours;
-    const baseCost = this.costs.find(
-      (cost) => cost.vehicleType === vehicle.vehicleType
-    );
-    console.log(`baseCost , ${baseCost}`);
-    const totalCost =
-      this.costs.find((cost) => cost.vehicleType === vehicle.vehicleType)!
-        ?.rate * hours;
+
+    const totalCost = this.rate(vehicle.vehicleType) * hours;
 
     console.log(
-      `You wer parked for ${hours} and the total cost is ${totalCost}`
+      `[Check-Out] You were parked for ${hours} and the total cost is ${totalCost}`
+    );
+    this.usedSpots = this.usedSpots.filter(
+      (spot) => spot.vehicle.plate !== vehicle.plate
     );
     return totalCost;
   };
 }
 
-const parkingLot: ParkingLot = new ParkingLot(1000, [
-  { vehicleType: "sedan", rate: 10 },
-  { vehicleType: "truck", rate: 10 },
-  { vehicleType: "bike", rate: 5 },
-]);
+const checkingTest = () => {
+  try {
+    const parkingLot: ParkingLot = new ParkingLot(1000, [
+      { vehicleType: "sedan", rate: 10 },
+      { vehicleType: "truck", rate: 10 },
+      { vehicleType: "bike", rate: 5 },
+    ]);
 
-const mazada3: Vehicle = new Vehicle("gdf123", "sedan");
+    const mazada3: Vehicle = new Vehicle("gdf123", "sedan");
 
-parkingLot.checkIn(mazada3);
-const cost = parkingLot.checkOut(mazada3);
+    parkingLot.checkIn(mazada3);
+    const cost = parkingLot.checkOut(mazada3);
+  } catch (error) {
+    console.error(`test failed with ${error}`);
+  }
+};
+//checkingTest();
+
+const noSpotsTest = () => {
+  try {
+    const parkingLot: ParkingLot = new ParkingLot(2, [
+      { vehicleType: "sedan", rate: 10 },
+      { vehicleType: "truck", rate: 10 },
+      { vehicleType: "bike", rate: 5 },
+    ]);
+
+    const mazada3: Vehicle = new Vehicle("gdsedan1f123", "sedan");
+    const bike: Vehicle = new Vehicle("bike1", "bike");
+    const truck: Vehicle = new Vehicle("truck1", "truck");
+    parkingLot.checkIn(mazada3);
+    parkingLot.checkIn(bike);
+    parkingLot.checkIn(truck);
+
+    const cost = parkingLot.checkOut(mazada3);
+  } catch (error) {
+    console.error(`test failed with ${error}`);
+  }
+};
+noSpotsTest();
+
+const repeatedVehicle = () => {
+  try {
+    const parkingLot: ParkingLot = new ParkingLot(2, [
+      { vehicleType: "sedan", rate: 10 },
+      { vehicleType: "truck", rate: 10 },
+      { vehicleType: "bike", rate: 5 },
+    ]);
+
+    const mazada3: Vehicle = new Vehicle("gdsedan1f123", "sedan");
+
+    parkingLot.checkIn(mazada3);
+    parkingLot.checkIn(mazada3);
+
+    const cost = parkingLot.checkOut(mazada3);
+  } catch (error) {
+    console.error(`test failed with ${error}`);
+  }
+};
+repeatedVehicle();
